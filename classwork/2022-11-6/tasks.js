aee58a278cb628576df93001d8ceb3e0b1e8681b
HussnulMaab192 <mabimalik192@gmail.com>
2022-11-13 12:02:08 -1200

    class work 9

---
+++ classwork/2022-11-6/tasks.js
@@ -0,0 +1,78 @@
+// program to reverse a string
+
+// function reverseString(str) {
+//   // empty string
+//   let newString = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     newString += str[i];
+//   }
+//   return newString;
+// }
+// console.log("🚀 ~ file: tasks.js ~ line 13 ~ con", reverseString("abc"));
+
+//
+
+// function reverse_a_number(n) {
+//   n = n + "";
+//   return n.split("").reverse().join("");
+// }
+// console.log(Number(reverse_a_number(32243)));
+
+//
+// console.log("🚀", "start ");
+// let response;
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+// console.log("end");
+
+// const abc = {
+//   a: "a",
+//   b: "b",
+//   c: { d: "d" },
+// };
+// const { b, ...re } = abc;
+// console.log(re), console.log(b);
+
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// console.log("🚀 ~ file: tasks.js ~ line 39 ~ arr", arr[2]);
+// [, , a, ...re] = arr; // it will give value a=3 and b = re = rst of value!
+// console.log(b);
+// console.log(a);
+// console.log(re);
+
+// First class function woh function jin ko ham as a variable use karain js main! read more !
+// function is_creditCard(abc) {
+//   regexp = /^[0-9]+(-[0-9]+)+$/;
+
+//   if (regexp.test(abc)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// const chkValid = is_creditCard("1234-1234-1234-1234");
+// chkValid ? console.log("Valid credit card") : console.log("Invalid");
+// var count = 0;
+
+// function countString(str) {
+//   for (let index = 0; index < str.length; index++) {
+//     if (index == " ") {
+//       count = count + 1;
+//     }
+//   }
+//   console.log(count);
+// }
+// countString("I am a man");
+
+// always talk precisely!
+// Always t
+
+console.log(abc);
+var abc = 10;
+var abc1 = 10;
+var abc2 = 10;
+var abc3 = 10;
+
+console.log(abc);
